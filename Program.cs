ec19e58ed8487a882634f8094095f35a5ccf05f2
Daniel <s-muhammed.anwer@zewailcity.edu.eg>
2021-05-21 01:10:26 +0200

    ffff

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace EducationalCenter
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form0());
+            Application.Run(new Form2E_A());
         }
     }
 }
